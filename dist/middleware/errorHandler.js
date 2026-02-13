96547fbbd6bb8c65c01c4af3ab75d31bcf0ac929
Yehuda Hason <yehudaxt@gmail.com>
2026-02-13 14:30:57 +0200

    prisma-js-client

--- dist/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-export function errorHandler(err, req, res, next) {
-    console.error(err); // log error
-    const statusCode = err.status || 500;
-    res.status(statusCode).json({
-        success: false,
-        message: err.message || "Internal Server Error",
-    });
-}
-//# sourceMappingURL=errorHandler.js.map
